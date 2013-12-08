db7017b67f055cca7f09ff96edc7e2f3e3ee93f9
malteh <nerv_nicht@gmx.net>
2013-12-08 15:09:41 +0100

    Aufrufe in Suche.pl

--- Suche.pl
+++ Suche.pl
@@ -5,4 +5,14 @@
 % :- consult('Suche_Modul_Wasser.pl').
 
 %%% Spezieller Teil: Planung
-:- consult('Suche_Modul_Planung_Vorlage.pl').
+:- consult('Suche_Modul_Planung.pl').
+
+%%% Aufrufe:
+%:- solve(depth, _).
+%:- solve(breadth, _).
+%:- solve(informed, good).
+%:- solve(informed, better).
+%:- solve(a,_).
+%:- solve(greedy,_).
+%:- solve(hc_optimistic,_).
+%:- solve(hc_backtracking,_).
